2b4a170e98286bf8fe64f5c3a4ae44e3c448a76f
MD Tafhim Faisal <tropical10tafhim@gmail.com>
2020-02-08 21:21:52 +0600

    fixed

--- views/admin/layouts/sidebar.php
+++ views/admin/layouts/sidebar.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
       
         <a href="index3.html" class="brand-link">
-          <img src="http://<?=$_SERVER['HTTP_HOST']?>/dist/img/bs-logo.png" alt="BS-23 Logo" class="brand-image img-circle elevation-3"
-              style="opacity: .8">
-          <span class="brand-text font-weight-light">BS-23 BloodBank</span>
+          <!-- <img src="http:///dist/img/bs-logo.png" alt=" Logo" class="brand-image img-circle elevation-3" -->
+              <!-- style="opacity: .8"> -->
+          <span class="brand-text font-weight-light">BloodBank</span>
         </a>
 
         <div class="sidebar">
